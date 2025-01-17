c26fb02c8c496518dcd2e9a9bd6fbca281d73bf5
qualwebs04.10 <rathorep.qualwebs@gmail.com>
2025-01-17 10:54:00 +0530

    Adding date picker

---
+++ src/app/date-picker/date-picker.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-date-picker',
+  templateUrl: './date-picker.component.html',
+  styleUrls: ['./date-picker.component.scss']
+})
+export class DatePickerComponent {
+  selectedDate: Date | undefined;
+}
