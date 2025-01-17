b2800763ac12d8c1009e80030e11f292cc5fbe2f
qualwebs04.10 <rathorep.qualwebs@gmail.com>
2025-01-17 11:06:31 +0530

    Added date picker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,12 @@ import {FirstscreenComponent} from './firstscreen/firstscreen.component';
 import {NgImageSliderModule} from 'ng-image-slider';
 import {DatePickerComponent} from './date-picker/date-picker.component';
 import {BsDatepickerModule} from 'ngx-bootstrap/datepicker';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [AppComponent, FirstscreenComponent, DatePickerComponent],
-  imports: [BrowserModule, AppRoutingModule, NgImageSliderModule, BsDatepickerModule.forRoot(), FormsModule,],
+  imports: [BrowserModule, AppRoutingModule, NgImageSliderModule, BsDatepickerModule.forRoot(),BrowserAnimationsModule, FormsModule,],
   providers: [],
   bootstrap: [AppComponent],
 })
